7f18cebf646580227def0128591a9511ae71c35c
Alek86 <bukaevav@gmail.com>
2014-10-06 12:18:19 -0400

    Added BreadthFirstSearch.cpp Not implemented though

---
+++ Algo/BreadthFirstSearch.cpp
@@ -0,0 +1,10 @@
+#include "BreadthFirstSearch.h"
+
+#include <exception>
+
+using namespace std;
+
+std::vector<size_t> BreadthFirstSearch( const Graph& graph, size_t fromVertex, size_t toVertex )
+{
+    throw std::exception();
+}
